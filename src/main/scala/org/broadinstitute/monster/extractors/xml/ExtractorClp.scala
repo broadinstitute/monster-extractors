7accdfece039784fa6a96e8c5d9e0b374fe3e8bd
Dan Moran <danmoran@broadinstitute.org>
2020-01-31 15:15:56 -0500

    Revert "Backport fix to blockers."
    This reverts commit 908f1cf12bb56d1952bec886be9e06b1def3ed6b.

--- src/main/scala/org/broadinstitute/monster/extractors/xml/ExtractorClp.scala
+++ src/main/scala/org/broadinstitute/monster/extractors/xml/ExtractorClp.scala
@@ -1,17 +1,12 @@
 package org.broadinstitute.monster.extractors.xml
 
-import java.util.concurrent.Executors
-
 import better.files.File
-import cats.data.{NonEmptyList, Validated, ValidatedNel}
-import cats.effect.{Blocker, ExitCode, IO, Resource}
+import cats.data.{Validated, ValidatedNel}
+import cats.effect.{Blocker, ExitCode, IO}
 import cats.implicits._
 import com.monovore.decline._
 import com.monovore.decline.effect._
 import org.broadinstitute.monster.MonsterXmlToJsonListBuildInfo
-
-import scala.concurrent.ExecutionContext
-import scala.util.control.NonFatal
 
 /**
   * Command-line program which can use our extractor functionality to convert
@@ -67,60 +62,16 @@ object ExtractorClp
 
     (inputOpt, outputOpt, gzipOpt, countOpt).mapN {
       case (in, out, gunzip, count) =>
-        // CE's default Blocker implementation hangs on fatal errors (i.e. OOM).
-        // Manually build a Blocker so we can inject the fix that's been merged,
-        // but not released: https://github.com/typelevel/cats-effect/pull/694.
-        val buildExecutorService =
-          IO.delay(Executors.newCachedThreadPool { runnable =>
-            val t = new Thread(runnable, "custom-blocker")
-            t.setDaemon(true)
-            t
-          })
-        Resource
-          .make(buildExecutorService) { es =>
-            val tasks = IO.delay {
-              val tasks = es.shutdownNow()
-              val builder = List.newBuilder[Runnable]
-              val itr = tasks.iterator()
-              while (itr.hasNext) {
-                builder += itr.next()
-              }
-              NonEmptyList.fromList(builder.result)
+        Blocker[IO].use { blocker =>
+          new XmlExtractor(blocker)
+            .extract(in, out, count, gunzip)
+            .compile
+            .last
+            .map {
+              case None    => ExitCode.Error
+              case Some(_) => ExitCode.Success
             }
-            tasks.flatMap {
-              case Some(t) => IO.raiseError(new Blocker.OutstandingTasksAtShutdown(t))
-              case None    => IO.unit
-            }
-          }
-          .map { es =>
-            // Delegate to the default EC, but intercept fatal errors.
-            new ExecutionContext {
-              private val base = ExecutionContext.fromExecutorService(es)
-              override def execute(runnable: Runnable): Unit =
-                base.execute { () =>
-                  try {
-                    runnable.run()
-                  } catch {
-                    case NonFatal(err) => reportFailure(err)
-                    case t: Throwable =>
-                      t.printStackTrace()
-                      System.exit(1)
-                  }
-                }
-              override def reportFailure(cause: Throwable): Unit =
-                base.reportFailure(cause)
-            }
-          }
-          .use { ec =>
-            new XmlExtractor(Blocker.liftExecutionContext(ec))
-              .extract(in, out, count, gunzip)
-              .compile
-              .last
-              .map {
-                case None    => ExitCode.Error
-                case Some(_) => ExitCode.Success
-              }
-          }
+        }
     }
   }
 }
